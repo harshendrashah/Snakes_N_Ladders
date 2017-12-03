fef7dd79663fb576b017d1d6ff62610d25aa6568
Harshendra Shah <harshendrashah812@gmail.com>
2017-12-03 18:28:43 +0530

    initial commit

---
+++ Snakes_and_Ladders/app/src/main/java/com/shah/snakes_and_ladders/MainActivity_Snakes_and_Ladders.java
@@ -0,0 +1,13 @@
+package com.shah.snakes_and_ladders;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_Snakes_and_Ladders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__snakes_and__ladders);
+    }
+}
